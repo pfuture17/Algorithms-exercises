799747b46f821892e0ad58cc9f6dae640eab9197
Future Dominique <77439365+pfuture17@users.noreply.github.com>
2021-11-09 10:35:39 -0500

    feat: reverse array in place

---
+++ reverseArrayInPlace.ts
@@ -0,0 +1,11 @@
+function reverseArrayInPlace(arr: number[]) {
+    for (var i = 0; i < arr.length / 2; i++) {
+        var tempVar = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = tempVar;
+    }
+
+    console.log(arr);
+}
+
+reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8]);
