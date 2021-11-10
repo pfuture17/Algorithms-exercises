c7170e122c998e471cd4b72df4bbc0df4ec40117
Future Dominique <77439365+pfuture17@users.noreply.github.com>
2021-11-10 09:30:47 -0500

    feat: twoSum

---
+++ twoSum.ts
@@ -0,0 +1,17 @@
+function twoSum(numArray: number[], sum: number) {
+    let pairs: number[][] = [];
+    let hashTable: number[] = [];
+
+    for (var i = 0; i < numArray.length; i++) {
+        let currNum = numArray[i];
+        let counterpart = sum - currNum;
+        if (hashTable.indexOf(counterpart) !== -1) {
+            pairs.push([currNum, counterpart]);
+        }
+        hashTable.push(currNum);
+    }
+
+    console.log(pairs);
+}
+
+twoSum([1, 6, 4, 5, 3, 3], 7);
